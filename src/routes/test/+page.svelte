e9cab666ee4292a1cd74dcc6bfab35052017743d
Rohit Bhagat <132160385+rb0125@users.noreply.github.com>
2026-03-19 22:52:49 -0400

    database branch

--- src/routes/test/+page.svelte
+++
@@ -1,7 +0,0 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/test">Test</a>
-</nav>
-
-<h1>Welcome to SvelteKit (Test)</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
