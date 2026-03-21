2d7004ac5df4135f99353f707848aa8bd75f3481
Rohit Bhagat <132160385+rb0125@users.noreply.github.com>
2026-03-20 21:30:26 -0400

    POST updates both sensor_events and sensors

--- src/routes/api/sensor-events/+server.js
+++ src/routes/api/sensor-events/+server.js
@@ -3,13 +3,13 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import { MONGO_URL } from "$env/static/private";
 
 export async function POST({ request }) {
+    // Get POST data
     const { sensor_id, timestamp, occupied } = await request.json();
-
-    console.log("Hello, World!");
     console.log("Sensor ID: " + sensor_id);
     console.log("Timestamp: " + timestamp);
     console.log("Occupied: " + occupied);
 
+    // Create database client
     const client = new MongoClient(MONGO_URL, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -18,19 +18,30 @@ export async function POST({ request }) {
     }
     });
 
+    // Add data to database
     try {
         await client.connect();
+        const db = client.db("ru_occupied");
+
         // Add sensor event to collection "sensor_events"
-        await client.db("ru_occupied").collection("sensor_events").insertOne({
+        await db.collection("sensor_events").insertOne({
             sensor_id: sensor_id,
             timestamp: new Date(timestamp),
             occupied: occupied
         });
-        // Also need to update current status of sensor in collection "sensors"
+
+        // Update current status of sensor in collection "sensors"
+        await db.collection("sensors").updateOne(
+            { _id: sensor_id },
+            { $set: { currently_occupied: occupied } },
+            { upsert: true }
+        );
+        
         console.log("Successfully inserted to MongoDB");
     }
     catch (error) {
         console.error("Error connecting to MongoDB: ", error);
+        return ({ status: 500 });
     }
     finally {
         await client.close();
